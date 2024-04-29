aaa4e2fe1b12ae1b0dcd78e525e0e2bf3cd64873
nomi3 <thisisnomi3@gmail.com>
2024-04-29 10:33:40 +0900

    add cron

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('test', function () {
+    Log::info('This is a test command');
+    $this->comment('This is a test command');
+})->purpose('Display a test command')->everyMinute();
